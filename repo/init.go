8efc7e73fc0dbcf5db3a0635d4a21ec36538425e
qitta <christoph@nullcat.de>
2015-11-19 22:33:29 +0100

    init.go: Obsolate suff removed. General function and updates.
    	* OpenGPG save, load and generate Keypair functions removed.
    	* FsRepository struct updated.
    	* NewFsRepository and LoadFsRepository updated.

--- repo/init.go
+++ repo/init.go
@@ -3,54 +3,13 @@ package repo
 import (
 	"code.google.com/p/go-uuid/uuid"
 	"fmt"
-	"github.com/ipfs/go-ipfs/repo/config"
+	ipfsconfig "github.com/ipfs/go-ipfs/repo/config"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
-	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/packet"
+	"github.com/olebedev/config"
 	"os"
 	"path"
 	"path/filepath"
 )
-
-func generateKeypair(name, comment, email string, cfg *packet.Config) (*openpgp.Entity, error) {
-	fmt.Println("Generating keypair...")
-	keypair, err := openpgp.NewEntity(name, comment, email, cfg)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("...done.")
-	return keypair, nil
-}
-
-func saveKeypair(path string, keypair *openpgp.Entity) error {
-	// TODO: Save key armored with a passphrase - currently not supported by
-	// golang openpgp library
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0755)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	keypair.SerializePrivate(f, nil) // TODO: packet.Config instead of nil?
-	return nil
-}
-
-func loadKeypair(path string) (*openpgp.Entity, error) {
-	// TODO: Save key armored keypair
-	f, err := os.OpenFile(path, os.O_RDONLY, 0755)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	enityList, err := openpgp.ReadKeyRing(f)
-	if err != nil {
-		return nil, err
-	}
-	if len(enityList) > 0 {
-		return enityList[0], nil
-	}
-	// should acually never be reached
-	return nil, err
-}
 
 // Repository interface for brig repository types
 type Repository interface {
@@ -62,17 +21,23 @@ type Repository interface {
 
 // FsRepository represents data a brig repository consists of
 type FsRepository struct {
-	Jid      string // name@domain.tld
-	Password string // sha-x representation
-	Folder   string // filesystem foldername repo is in
-	UID      string
 
-	ConfigPath string
+	// Repository is identified by a XMPP Account: name@domain.tld/ressource
+	Jid string
 
-	// Crypto
-	GPGKeypair *openpgp.Entity
-	AesKey     string
-	OtrKey     string
+	// Minilock ID
+	Mid string
+
+	// Folder of repository
+	Folder string
+
+	// UUID which represents a unique repository
+	UniqueID string
+
+	// TODO: Just for prototype testing, should be deleted in final version
+	Password string
+
+	Config *config.Config
 }
 
 // Interface methods
@@ -99,7 +64,7 @@ func (r *FsRepository) Unlock() {
 
 // NewFsRepository creates a new repository at filesystem level
 // and returns a Repository interface
-func NewFsRepository(jid, pass, folder string) (Repository, error) {
+func NewFsRepository(jid, pass, folder string) (*FsRepository, error) {
 
 	absFolderPath, err := filepath.Abs(folder)
 	if err != nil {
@@ -114,20 +79,74 @@ func NewFsRepository(jid, pass, folder string) (Repository, error) {
 		return nil, err
 	}
 
-	repoUID := uuid.NewRandom()
-	repo := FsRepository{
-		Jid:        jid,
-		Password:   pass,
-		Folder:     absFolderPath,
-		UID:        repoUID.String(),
-		ConfigPath: path.Join(folder, ".brig", "config"),
+	cfg := CreateDefaultConfig()
+
+	minilockID, err := GenerateMinilockID(jid, pass)
+	if err != nil {
+		return nil, err
 	}
-	return &repo, nil
+	configDefaults := map[string]interface{}{
+		"repository.jid":      jid,
+		"repository.password": pass,
+		"repository.uuid":     uuid.NewRandom().String(),
+		"repository.mid":      minilockID,
+		"ipfs.path":           path.Join(absFolderPath, ".brig", "ipfs"),
+	}
+
+	for key, value := range configDefaults {
+		if err = cfg.Set(key, value); err != nil {
+			return nil, err
+		}
+	}
+
+	if _, err := SaveConfig(path.Join(absFolderPath, ".brig", "config"), cfg); err != nil {
+		return nil, err
+	}
+
+	return LoadFsRepository(absFolderPath)
 }
 
 // CloneFsRepository clones a brig repository in a git like way
-func CloneFsRepository() *Repository {
+func CloneFsRepository() *FsRepository {
 	return nil
+}
+
+// LoadFsRepository load a brig repository from a given folder.
+func LoadFsRepository(folder string) (*FsRepository, error) {
+	absFolderPath, err := filepath.Abs(folder)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := LoadConfig(path.Join(absFolderPath, ".brig", "config"))
+	if err != nil {
+		return nil, err
+	}
+
+	configValues := map[string]string{
+		"repository.jid":      "",
+		"repository.mid":      "",
+		"repository.uuid":     "",
+		"repository.password": "",
+	}
+
+	for key := range configValues {
+		configValues[key], err = cfg.String(key)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	repo := FsRepository{
+		Jid:      configValues["repository.jid"],
+		Mid:      configValues["repository.mid"],
+		Password: configValues["repository.password"],
+		Folder:   absFolderPath,
+		UniqueID: configValues["repository.uuid"],
+		Config:   cfg,
+	}
+
+	return &repo, nil
 }
 
 func createRepositoryTree(absFolderPath string) error {
@@ -147,7 +166,7 @@ func createRepositoryTree(absFolderPath string) error {
 }
 
 func createIPFS(ipfsRootPath string) error {
-	cfg, err := config.Init(os.Stdout, 2048)
+	cfg, err := ipfsconfig.Init(os.Stdout, 2048)
 	if err != nil {
 		return err
 	}
@@ -158,23 +177,3 @@ func createIPFS(ipfsRootPath string) error {
 
 	return nil
 }
-
-//func main() {
-//	//_, err := NewFsRepository(os.Args[1], os.Args[2], os.Args[3])
-//	//if err != nil {
-//	//	fmt.Println(err)
-//	//	os.Exit(3)
-//	//}
-//	e, err := loadKeypair("catz")
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	for _, subkey := range e.Subkeys {
-//		if subkey.PrivateKey != nil {
-//			fmt.Printf("Privater Schluessel % x\n", subkey.PrivateKey.Fingerprint)
-//		}
-//		if subkey.PublicKey != nil {
-//			fmt.Printf("Public Schluessel % x\n", subkey.PublicKey.Fingerprint)
-//		}
-//	}
-//}
