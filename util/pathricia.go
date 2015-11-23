985a3218c0f29353b73c73c7dfbaba62e871b8f0
Chris P <sahib@online.de>
2015-11-23 11:41:36 +0100

    pathtricia: Extend the API a tad.

--- util/pathricia.go
+++ util/pathricia.go
@@ -1,34 +1,72 @@
+// This package implements a general purpose Path-Trie.
 package util
 
 import (
-	"fmt"
 	"os"
 	"strings"
 )
 
+// Node represents a single node in a Trie, but it can be used as a whole
+// (sub-)Trie through the Trie interface. A node value of `nil` is a perfectly
+// valid trie. Node is suitable for embedding it into other structs.
 type Node struct {
-	Parent   *Node
+	// Pointer to parent node or nil
+	Parent *Node
+
+	// Basename to child-nodes
 	Children map[string]*Node
-	Name     string
-	NodeSize int64
+
+	// Basename of the node's Path
+	Name string
+
+	// Number of explicitly added children of this node.
+	// (1 for leaf nodes)
+	Length int64
+
+	// Depth of the node. The root is at depth 0.
+	Depth uint16
 }
 
+// Trie represents the required methods for accessing a directory structure.
 type Trie interface {
+	// Root returns the uppermost node of the trie.
 	Root() *Node
 
-	Add(string) *Node
+	// Insert adds a new node in the trie at string. If the node already exists,
+	// nothing changes. This operation costs O(log(n)). The newly created or
+	// existant node is returned.
+	Insert(path string) *Node
 
-	Lookup(string) *Node
+	// Lookup searches for a node references by a path.
+	Lookup(path string) *Node
 
-	Remove(string) *Node
+	// Remove removes the node at path and all of it's children.
+	// The parent of the removed node is returned, which might be nil.
+	Remove() *Node
 
-	Size() int64
+	// Len returns the current number of elements in the trie.
+	// This counts only explicitly inserted Nodes.
+	Len() int64
 }
 
+// SplitPath splits the path according to os.PathSeparator,
+// but omits a leading empty name on /unix/paths
+func SplitPath(path string) []string {
+	names := strings.Split(path, string(os.PathSeparator))
+	if len(names) > 0 && names[0] == "" {
+		return names[1:]
+	}
+
+	return names
+}
+
+// NewTrie returns a trie with the root element pre-inserted.
+// Note that `nil` is a perfectly valid, but empty trie.
 func NewTrie() *Node {
-	return &Node{NodeSize: 0}
+	return &Node{}
 }
 
+// Root returns the root node of the trie.
 func (n *Node) Root() *Node {
 	if n != nil && n.Parent != nil {
 		return n.Parent.Root()
@@ -36,91 +74,109 @@ func (n *Node) Root() *Node {
 	return n
 }
 
-func (n *Node) Add(path string) *Node {
-	names := strings.Split(path, string(os.PathSeparator))
+// Insert adds a node into the trie at `path`
+func (n *Node) Insert(path string) *Node {
 	curr := n
 	if curr == nil {
-		curr = &Node{NodeSize: 0}
+		curr = &Node{}
 	}
 
-	for _, name := range names {
+	wasAdded := false
+
+	for depth, name := range SplitPath(path) {
 		if curr.Children == nil {
 			curr.Children = make(map[string]*Node)
 		}
 		child, ok := curr.Children[name]
 		if !ok {
 			child = &Node{
-				Parent:   curr,
-				Children: nil,
-				Name:     name,
-				NodeSize: 1,
+				Parent: curr,
+				Name:   name,
+				Depth:  uint16(depth + 1),
 			}
 			curr.Children[name] = child
-			curr.Up(func(parent *Node) {
-				parent.NodeSize++
-			})
+			wasAdded = true
 		}
 		curr = child
 	}
 
+	if wasAdded {
+		curr.Up(func(parent *Node) {
+			parent.Length++
+		})
+	}
 	return curr
 }
 
+// Lookup searches a Node by it's absolute path.
 func (n *Node) Lookup(path string) *Node {
+	curr := n
 	if n == nil {
 		return nil
 	}
-	names := strings.Split(path, string(os.PathSeparator))
-	curr := n
-	for _, name := range names {
-		if child, ok := curr.Children[name]; !ok {
+
+	for _, name := range SplitPath(path) {
+		child, ok := curr.Children[name]
+		if !ok {
 			return nil
-		} else {
-			curr = child
 		}
+		curr = child
 	}
 	return curr
 }
 
-func (n *Node) Remove(path string) *Node {
-	node := n.Lookup(path)
-	if node == nil {
+// Remove removes the receiver and all of it's children.
+// The removed node's parent is returned.
+func (n *Node) Remove() *Node {
+	if n == nil {
 		return nil
 	}
 
-	// Adjusts the parent's size.
-	size := node.NodeSize
-	node.Up(func(parent *Node) {
-		parent.NodeSize -= size
+	// Adjusts the parent's length:
+	length := n.Length
+	n.Up(func(parent *Node) {
+		parent.Length -= length
 	})
 
-	// Removes link to self.
-	if node.Parent != nil {
-		delete(node.Parent.Children, node.Name)
+	// Removes link to self:
+	if n.Parent != nil {
+		delete(n.Parent.Children, n.Name)
 	}
 
-	// Make children garbage collectable.
-	parent := node.Parent
-	node.Iterate(func(child *Node) {
+	// Make children garbage collectable:
+	parent := n.Parent
+	n.Walk(true, func(child *Node) {
 		child.Children = nil
 		child.Parent = nil
 	})
 	return parent
 }
 
-func (n *Node) Iterate(visit func(*Node)) {
+// Walk iterates over all (including intermediate )nodes in the trie.
+// Depending on dfs the nodes are visited in depth-first or breadth-first.
+// The supplied callback is called for each visited node.
+func (n *Node) Walk(dfs bool, visit func(*Node)) {
 	if n == nil {
 		return
 	}
 
+	if !dfs {
+		visit(n)
+	}
+
 	if n.Children != nil {
 		for _, child := range n.Children {
-			child.Iterate(visit)
+			child.Walk(dfs, visit)
 		}
 	}
-	visit(n)
+
+	if dfs {
+		visit(n)
+	}
 }
 
+// Up walks from the receiving node to the root node,
+// calling `visit` on each node on it's way.
 func (n *Node) Up(visit func(*Node)) {
 	if n != nil {
 		visit(n)
@@ -128,16 +184,38 @@ func (n *Node) Up(visit func(*Node)) {
 	}
 }
 
-func (n *Node) Size() int64 {
+// Len returns the number of explicitly inserted elements in the trie.
+func (n *Node) Len() int64 {
 	if n == nil {
 		return 0
 	}
-	return n.NodeSize
+	return n.Length
 }
 
+// Path returns a full absolute path from the receiver
+// to the root node of the trie.
+func (n *Node) Path() string {
+	if n == nil {
+		return ""
+	}
+
+	s := make([]string, n.Depth+1)
+	i := len(s) - 1
+
+	n.Up(func(parent *Node) {
+		s[i] = parent.Name
+		i--
+	})
+
+	// TODO: Return "/" if it's the root node only.
+	//       But don't do that on windows?
+	return strings.Join(s, string(os.PathSeparator))
+}
+
+// String returns the absolute path of the node.
 func (n *Node) String() string {
 	if n == nil {
 		return "<nil>"
 	}
-	return fmt.Sprintf("[%p:%s](N: %d)", n, n.Name, n.NodeSize)
+	return n.Path()
 }
--- util/pathricia_test.go
+++ util/pathricia_test.go
@@ -5,18 +5,39 @@ import (
 	"testing"
 )
 
-func TestPathricia(t *testing.T) {
+func TestPathriciaLinux(t *testing.T) {
 	trie := NewTrie()
-	fmt.Println(trie.Size())
+	fmt.Println(trie.Len())
 
-	n := trie.Add("/home/qitta")
+	n := trie.Insert("/home/qitta")
 	fmt.Println(n, n.Root(), n.Parent)
-	n = trie.Add("/home/sahib/gat/georg")
+	n = trie.Insert("/home/sahib/gat/georg")
 	fmt.Println(n, n.Root(), n.Parent)
-	n = trie.Remove("/home/sahib/gat/georg")
+	fmt.Println("Whole trie:")
+
+	trie.Walk(false, func(child *Node) {
+		fmt.Println("  :", child)
+	})
+
+	fmt.Println("----", SplitPath("/home/qitta"))
+
+	n = trie.Lookup("/home/sahib/gat/georg").Remove()
 	fmt.Println(n, n.Root(), n.Parent)
-	n = trie.Remove("/home")
+	n = trie.Lookup("/home").Remove()
 	fmt.Println(n, n.Root(), n.Parent)
-	n = trie.Remove("/")
+	n = trie.Lookup("/").Remove()
 	fmt.Println(n, n.Root(), n)
+
+	var x *Node = nil
+	fmt.Println(x.Path())
+}
+
+func TestPathriciaWindows(t *testing.T) {
+	// TODO: This fails:
+	trie := NewTrie()
+	n := trie.Insert("C:/Albert")
+	fmt.Println(n)
+	trie.Walk(false, func(child *Node) {
+		fmt.Println("  :", child)
+	})
 }
