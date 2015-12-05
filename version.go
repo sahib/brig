f35085dd86e8c8e1c67c96565e8c3859e81e8503
Chris P <sahib@online.de>
2015-12-05 01:39:48 +0100

    version: Make it look a bit more like SemVer

--- version.go
+++ version.go
@@ -3,20 +3,22 @@ package brig
 import "fmt"
 
 const (
-	// MajorVersion will be incremented on big releases.
-	MajorVersion = 0
-	// MinorVersion will be incremented on small releases.
-	MinorVersion = 0
-	// PatchVersion should be incremented on every released change.
-	PatchVersion = 0
+	// Major will be incremented on big releases.
+	Major = 0
+	// Minor will be incremented on small releases.
+	Minor = 0
+	// Patch should be incremented on every released change.
+	Patch = 0
+	// PreRelease is an empty string for final releases, {alpha,beta} for pre-releases.
+	PreRelease = "beta"
 )
 
 // Version returns a tuple of (major, minor, patch)
 func Version() (int, int, int) {
-	return MajorVersion, MinorVersion, PatchVersion
+	return Major, Minor, Patch
 }
 
 // VersionString returns a Maj.Min.Patch string.
 func VersionString() string {
-	return fmt.Sprintf("%d.%d.%d", MajorVersion, MinorVersion, PatchVersion)
+	return fmt.Sprintf("v%d.%d.%d-%s", Major, Minor, Patch, PreRelease)
 }
