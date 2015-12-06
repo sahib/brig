e793c4fffbec04af3510811f9fe683023a428906
qitta <christoph@nullcat.de>
2015-12-06 22:11:12 +0100

    pathricia_test.go: Relative node insertion test.

--- util/trie/pathricia_test.go
+++ util/trie/pathricia_test.go
@@ -45,3 +45,46 @@ func TestPathriciaInsertTrieLinux(t *testing.T) {
 
 	}
 }
+
+func TestPathriciaInsertRelativeLinux(t *testing.T) {
+	tests := []struct {
+		input  string
+		name   string
+		path   string
+		length int64
+	}{
+		//Insert path | expected node name | expected path | path len
+		{"", "", "/", 0},
+		{"/", "", "/", 1},
+		{"a", "a", "/a", 2},
+		{"b", "b", "/a/b", 3},
+		{"c", "c", "/a/b/c", 4},
+	}
+
+	trie := NewTrie()
+	node := trie.Root()
+	for _, test := range tests {
+
+		// Inserting at always at the returned node.
+		node = node.Insert(test.input)
+		if node == nil {
+			t.Errorf("Node is nil.", test)
+			continue
+		}
+
+		// Check the explicitly added paths.
+		nodeLen := trie.Length
+		if nodeLen != test.length {
+			t.Errorf("Length differs, got: %d != expected: %d\n", nodeLen, test.length)
+		}
+
+		if node.Name != test.name {
+			t.Errorf("Name differs, got: %s != expected: %s\n", node.Name, test.name)
+		}
+
+		if node.Path() != test.path {
+			t.Errorf("Path differs, got: %s != expected: %s\n", node.Path(), test.path)
+		}
+	}
+
+}
