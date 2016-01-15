6242898f54e2076f03d8871dbf67caaa20dd0691
Chris P <sahib@online.de>
2016-01-15 02:06:43 +0100

    rebuilding site Fr 15. Jan 02:06:43 CET 2016

---
+++ doc/blog/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+cd ../../
+git push gitlab master
+git subtree push --prefix=public gitlab gh-pages
