37fcb6878dba66145808c5904490d5eaddc46962
qitta <christoph@nullcat.de>
2016-02-16 18:01:24 +0100

    reader.go: Code cleanup. Block redefined to Record.

--- store/compress/compressor/reader.go
+++ store/compress/compressor/reader.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"sort"
@@ -17,7 +16,7 @@ import (
 type reader struct {
 	rawR    io.ReadSeeker
 	zipR    io.Reader
-	index   []Block
+	index   []Record
 	readBuf *bytes.Buffer
 	trailer *Trailer
 }
@@ -27,18 +26,20 @@ func (r *reader) Seek(offset int64, whence int) (int64, error) {
 }
 
 // Return start (prev offset) and end (curr offset) of the block currOff is
-// located in. If currOff is 0, the startoffset of the first and second block is
+// located in. If currOff is 0, the startoffset of the first and second record is
 // returned. If currOff is at the end of file the end offset of the last block
-// is returned twice.  The difference between prev block and curr block is then
+// is returned twice.  The difference between prev record and curr block is then
 // equal to 0.
-func (r *reader) blockLookup(currOff int64) (*Block, *Block) {
+func (r *reader) blockLookup(currOff int64) (*Record, *Record) {
 	i := sort.Search(len(r.index), func(i int) bool {
 		return r.index[i].zipOff > currOff
 	})
+
 	// Beginning of the file, first block: prev offset is 0, curr offset is 1
 	if i == 0 {
 		return &r.index[i], &r.index[i+1]
 	}
+
 	// End of the file, last block: prev and curr offset is the last index.
 	if i == len(r.index) {
 		return &r.index[i-1], &r.index[i-1]
@@ -46,7 +47,6 @@ func (r *reader) blockLookup(currOff int64) (*Block, *Block) {
 	return &r.index[i-1], &r.index[i]
 }
 
-//TODO: Clean code.
 func (r *reader) parseHeaderIfNeeded() error {
 	if r.trailer != nil {
 		return nil
@@ -60,94 +60,85 @@ func (r *reader) parseHeaderIfNeeded() error {
 	if n, err := r.rawR.Read(buf[:]); err != nil || n != TrailerSize {
 		return err
 	}
-
 	r.trailer = &Trailer{}
 	r.trailer.unmarshal(buf[:])
-	trailerBuf := make([]byte, r.trailer.indexSize)
 
-	var err error
 	seekIdx := -(int64(r.trailer.indexSize) + TrailerSize)
-	if _, err = r.rawR.Seek(seekIdx, os.SEEK_END); err != nil {
-		fmt.Println(err)
+	if _, err := r.rawR.Seek(seekIdx, os.SEEK_END); err != nil {
 		return err
 	}
 
+	trailerBuf := make([]byte, r.trailer.indexSize)
 	if _, err := r.rawR.Read(trailerBuf); err != nil {
-		fmt.Println(err)
 		return err
 	}
 
-	//Build Index
-	prevBlock := Block{-1, -1}
+	// Build index with Records. A record encapsulates a raw offset and the
+	// compressed offset it is mapped to.
+	prevRecord := Record{-1, -1}
 	for i := uint64(0); i < (r.trailer.indexSize / IndexBlockSize); i++ {
-		currBlock := Block{}
-		currBlock.unmarshal(trailerBuf)
+		currRecord := Record{}
+		currRecord.unmarshal(trailerBuf)
 
-		if prevBlock.rawOff >= currBlock.rawOff && prevBlock.zipOff >= currBlock.zipOff {
+		if prevRecord.rawOff >= currRecord.rawOff && prevRecord.zipOff >= currRecord.zipOff {
 			return ErrBadIndex
 		}
 
-		r.index = append(r.index, currBlock)
+		r.index = append(r.index, currRecord)
 		trailerBuf = trailerBuf[IndexBlockSize:]
 	}
 
 	if _, err := r.rawR.Seek(0, os.SEEK_SET); err != nil {
 		return err
 	}
-
 	return nil
 }
 
 func (r *reader) Read(p []byte) (int, error) {
 	if err := r.parseHeaderIfNeeded(); err != nil {
-		fmt.Println(err)
 		return 0, err
 	}
 
 	read := 0
 	for {
-		fmt.Println("READBUF LEN:", r.readBuf.Len())
 		if r.readBuf.Len() != 0 {
 			n := copy(p, r.readBuf.Next(len(p)))
 			read += n
 			p = p[n:]
-			fmt.Println("P:", len(p))
 		}
+
 		if len(p) == 0 {
 			break
 		}
 
-		if _, err := r.readBlockBuffered(); err != nil {
-			fmt.Println("EOF?", read, err)
+		if _, err := r.readBlock(); err != nil {
 			return read, err
 		}
 	}
-	fmt.Println("END:", read)
+
 	return read, nil
 }
 
-// TODO: Ist das noch "buffered"?
-func (r *reader) readBlockBuffered() (int64, error) {
-	// Get current raw position
+func (r *reader) readBlock() (int64, error) {
+	// Get current position of reader (offset of the compressed file).
 	currOff, err := r.rawR.Seek(0, os.SEEK_CUR)
 	if err != nil {
 		return 0, err
 	}
 
-	// Get compressed offset and set cursor to that position.
-	prevBlock, currBlock := r.blockLookup(currOff)
-	fmt.Println("#########################", currOff, prevBlock, currBlock)
-	if currBlock == nil || prevBlock == nil {
+	// Get the start and end record of the block currOff is located in
+	prevRecord, currRecord := r.blockLookup(currOff)
+	if currRecord == nil || prevRecord == nil {
 		return 0, ErrBadIndex
 	}
 
 	// Blocksize should only be 0 on empty file or at the end of file.
-	blockSize := currBlock.rawOff - prevBlock.rawOff
+	blockSize := currRecord.rawOff - prevRecord.rawOff
 	if blockSize == 0 {
 		return 0, io.EOF
 	}
 
-	currZipOff := prevBlock.zipOff
+	currZipOff := prevRecord.zipOff
 	if _, err = r.rawR.Seek(currZipOff, os.SEEK_SET); err != nil {
 		return 0, err
 	}
@@ -155,6 +146,9 @@ func (r *reader) readBlockBuffered() (int64, error) {
 	return io.CopyN(r.readBuf, r.zipR, blockSize)
 }
 
+// Return a new ReadSeeker with compression support. As random access is the
+// purpose of this layer, a ReadSeeker is required as parameter. The used
+// compression algorithm is chosen based on trailer information.
 func NewReader(r io.ReadSeeker) io.ReadSeeker {
 	return &reader{
 		rawR:    r,
