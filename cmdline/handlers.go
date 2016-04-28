b33fa7bea01534b25e122fda33b8b0da9fa2f1e3
qitta <christoph@nullcat.de>
2016-04-28 18:24:56 +0200

    cmdline/handlers.go: Remote handlers updated.

--- cmdline/handlers.go
+++ cmdline/handlers.go
@@ -591,14 +591,75 @@ func handleRemoteAdd(ctx climax.Context, client *daemon.Client) int {
 	return Success
 }
 
+func handleRemoteRemove(ctx climax.Context, client *daemon.Client) int {
+	idString := ctx.Args[2]
+	id, err := id.Cast(idString)
+	if err != nil {
+		log.Errorf("Invalid ID: %v", err)
+		return BadArgs
+	}
+
+	if client.RemoteRemove(id) != nil {
+		log.Errorf("Unable to remove remote: %v", err)
+		return UnknownError
+	}
+
+	return Success
+}
+
 func handleRemoteList(ctx climax.Context, client *daemon.Client) int {
-	list, err := client.RemoteList()
+	data, err := client.RemoteList()
 	if err != nil {
 		log.Errorf("Unable to list remotes: %v", err)
 		return UnknownError
 	}
+	for _, entry := range data {
+		printRemoteEntry(entry)
+	}
+	return Success
+}
 
-	fmt.Println(list)
+func handleRemoteLocate(ctx climax.Context, client *daemon.Client) int {
+	if len(ctx.Args) < 2 {
+		return BadArgs
+	}
+
+	id, err := id.Cast(ctx.Args[1])
+	if err != nil {
+		log.Errorf("Invalid ID: %v", err)
+		return BadArgs
+	}
+
+	hashes, err := client.RemoteLocate(id, 10, 50000)
+	if err != nil {
+		log.Errorf("Unable to locate ipfs peers: %v", err)
+		return UnknownError
+	}
+
+	for _, hash := range hashes {
+		fmt.Println(hash)
+	}
+
+	return Success
+}
+
+func printRemoteEntry(re *daemon.RemoteEntry) {
+	state := colors.Colorize("offline", colors.Red)
+	if re.IsOnline {
+		state = colors.Colorize("online", colors.Green)
+	}
+
+	fmt.Printf("%s %-7s %s\n", re.Hash, state, re.Ident)
+}
+
+func handleRemoteSelf(ctx climax.Context, client *daemon.Client) int {
+	re, err := client.RemoteSelf()
+	if err != nil {
+		log.Errorf("Unable to list remote self information: %v", err)
+		return UnknownError
+	}
+
+	printRemoteEntry(re)
 	return Success
 }
 
@@ -607,15 +668,21 @@ func handleRemote(ctx climax.Context, client *daemon.Client) int {
 		return handleRemoteList(ctx, client)
 	}
 
+	fmt.Println(ctx.Args[0])
 	switch ctx.Args[0] {
 	case "add":
 		return handleRemoteAdd(ctx, client)
 	// TODO!
 	case "remove":
+		return handleRemoteRemove(ctx, client)
 	case "list":
 		return handleRemoteList(ctx, client)
 	case "locate":
+		return handleRemoteLocate(ctx, client)
 	case "self":
+		return handleRemoteSelf(ctx, client)
+	default:
+		return handleRemoteSelf(ctx, client)
 	}
 
 	log.Warningf("No remote subcommand `%s`", ctx.Args[0])
