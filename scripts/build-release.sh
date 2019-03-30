1c9f62cb0bf179d5ba0e35edd62c2e373bda783e
Chris Pahl <sahib@online.de>
2019-03-31 00:14:33 +0100

    scripts: fix release builder

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 BIN_DIR="/tmp/brig-binaries"
 PLATFORMS="linux/amd64 linux/386 linux/arm darwin/amd64 darwin/386 freebsd/arm freebsd/386 freebsd/amd64"
 
@@ -16,7 +14,8 @@ build_all() {
 		local log_out="/tmp/build_${log}_${arch}.log"
 
         echo "-- Building brig_${os}_${arch}"
-        BRIG_BINARY_PATH="${BIN_DIR}/brig_${os}_${arch}" GOOS=$os GOARCH=$arch make build-small &> ${log_out}
+        go build -o /tmp/mage mage.go
+        BRIG_BINARY_PATH="${BIN_DIR}/brig_${os}_${arch}" GOOS=$os GOARCH=$arch /tmp/mage &> ${log_out}
         if [ $? != 0 ]; then
             echo "-- FAILED:"
             cat ${log_out}
