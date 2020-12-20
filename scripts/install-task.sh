3f613f0443f69a82bd15a4c9fd32930f79d466a3
Chris Pahl <sahib@online.de>
2020-12-20 15:15:35 +0100

    travis: always use go.mod

--- scripts/install-task.sh
+++ scripts/install-task.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 sh -c "$(curl -ssL https://taskfile.dev/install.sh)" -- -d
 
 if [ "$EUID" -ne 0 ]; then
+    echo '-- Not running as root, so putting binary in ./bin/task'
+else
     cp bin/task /usr/bin
     rm -rf bin
-else
-    echo '-- Not running as root, so putting binary in ./bin/task'
 fi
