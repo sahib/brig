a118c477444cda3e3333d08a8f7c198b561a0c5a
Eugeniy E. Mikhailov <evgmik@gmail.com>
2021-01-10 21:38:54 -0500

    generate.sh reports about missing commands

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -16,13 +16,16 @@ go mod download
 
 INCLUDE_PATH="$(go list -f '{{ .Dir }}' zombiezen.com/go/capnproto2)"
 
+command -v capnp > /dev/null && \
 for capnp_path in "${capnp_paths[@]}"
 do
     echo "-- Generating ${capnp_path}"
     capnp compile \
         -I"${INCLUDE_PATH}/std" \
         -ogo "${capnp_path}"
-done
+done \
+|| echo "Missing 'capnp' command, the build could be incomplete"
 
 
-go generate ./...
+command -v parcello > /dev/null && \
+go generate ./... || echo "Missing 'parcello' command, the build could be incomplete"
